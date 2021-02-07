c583554f98d5d8c1b7c724a1f0cff88a37305bb0
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-07 17:47:36 -0300

    Enabled slice the result of keys and values

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const assert = require('assert');
 
 class BinaryTree extends Array {
+  constructor() {
+    super();
+  }
+
   update(data) {
     let self = this;
     if (typeof(data) === 'object') {
@@ -13,6 +17,26 @@ class BinaryTree extends Array {
   len() {
     return Object.keys(this).length;
   }
+
+  keys(min, max) {
+    let out = Object.keys(this);
+    if ((min !== undefined && max !== undefined)) {
+      out = out.slice(min, max);
+    } else if ((min !== undefined && max === undefined)) {
+      out = out.slice(min);
+    }
+    return out;
+  }
+
+  values(min, max) {
+    let out = Object.values(this);
+    if ((min !== undefined && max !== undefined)) {
+      out = out.slice(min, max);
+    } else if ((min !== undefined && max === undefined)) {
+      out = out.slice(min);
+    }
+    return out;
+  }
 }
 
 assert.ok((new BinaryTree()) instanceof BinaryTree);
@@ -20,3 +44,9 @@ let bt = new BinaryTree();
 bt.update({ 1: 'bee', 2: 'gnu', 3: 'tiger', 4: 'elephant' });
 assert.equal(bt.len(), 4);
 assert.equal(bt[1], 'bee');
+assert.deepEqual(bt.keys(), [1, 2, 3, 4]);
+assert.deepEqual(bt.keys(1, 3), [2, 3]);
+assert.deepEqual(bt.keys(2), [3, 4]);
+assert.deepEqual(bt.values(), ['bee', 'gnu', 'tiger', 'elephant']);
+assert.deepEqual(bt.values(1, 3), ['gnu', 'tiger']);
+assert.deepEqual(bt.values(2), ['tiger', 'elephant']);
